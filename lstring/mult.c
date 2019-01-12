f8719d309adc9cc8c9c110be12452231eba9beba
Peter-Jacob <46247351+Peter-Jacob@users.noreply.github.com>
2019-01-12 13:57:06 +0100

    test for potential overflow

--- lstring/mult.c
+++ lstring/mult.c
@@ -15,22 +15,34 @@
  *
  */
 
+#include "lerror.h"
 #include "lstring.h"
 
 /* ------------------- Lmult ----------------- */
 void __CDECL
 Lmult( const PLstr to, const PLstr A, const PLstr B)
 {
-	L2NUM(A);
-	L2NUM(B);
+#if defined(__CMS__) || defined(__MVS__)
+   if (A->len+B->len>LMAXNUMERICSTRING) {
+      fprintf(STDERR,"\nMULTIPLAY OVERFLOW");
+/*    Lerror(ERR_ARITH_OVERFLOW,1); */
+      if (A->len>B->len) LREAL(*to) = TOREAL(*A);
+         else LREAL(*to) = TOREAL(*B);
+        LTYPE(*to) = LREAL_TY;
+        LLEN(*to)  = sizeof(double);
+      return;
+    }
+#endif
+    L2NUM(A);
+    L2NUM(B);
 
-	if ((LTYPE(*A)==LINTEGER_TY) && (LTYPE(*B)==LINTEGER_TY)) {
-		LINT(*to)  = LINT(*A) * LINT(*B);
-		LTYPE(*to) = LINTEGER_TY;
-		LLEN(*to)  = sizeof(long);
-	} else {
-		LREAL(*to) = TOREAL(*A) * TOREAL(*B);
-		LTYPE(*to) = LREAL_TY;
-		LLEN(*to)  = sizeof(double);
-	}
+    if ((LTYPE(*A)==LINTEGER_TY) && (LTYPE(*B)==LINTEGER_TY)) {
+        LINT(*to)  = LINT(*A) * LINT(*B);
+        LTYPE(*to) = LINTEGER_TY;
+        LLEN(*to)  = sizeof(long);
+    } else {
+        LREAL(*to) = TOREAL(*A) * TOREAL(*B);
+        LTYPE(*to) = LREAL_TY;
+        LLEN(*to)  = sizeof(double);
+    }
 } /* Lmult */
