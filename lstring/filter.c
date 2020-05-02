e258ee8325c53e10479db384b1653478bdf98c8e
Peter-Jacob <46247351+Peter-Jacob@users.noreply.github.com>
2020-05-02 10:46:07 +0200

    added new function: Filter

---
+++ lstring/filter.c
@@ -0,0 +1,30 @@
+//
+// Created by PeterJ on 30.04.2020.
+// Drops all characters defined in input table from string
+//
+
+#include "lstring.h"
+
+/* ---------------- Lfilter ------------------- */
+void __CDECL
+Lfilter( const PLstr to, const PLstr from, const PLstr tablein) {
+    int i, j, k=-1 ;
+
+    Lstrcpy(to, from);
+    L2STR(to);
+    L2STR(tablein);
+
+    if (LLEN(*tablein) == 0 || LLEN(*to) == 0)  { return ;}  // nothing to change
+// Analysis of string, drop chars which are in input table
+    for (i = 0; i < LLEN(*to); i++) {
+        for (j = 0; j < LLEN(*tablein); j++) {
+            if (LSTR(*to)[i] == LSTR(*tablein)[j]) {goto dropChar ;}  // drop char the fast way
+        }
+        k++;                          // set to next character position
+        LSTR(*to)[k] = (byte) LSTR(*to)[i]; // transfer char as relevant
+        dropChar: ;
+     }
+// String analysis completed
+    k++;                // set to real length (+1)
+    LLEN(*to) = (size_t) k;
+} /* Lfilter */
