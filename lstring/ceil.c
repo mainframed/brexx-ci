6fe4fdc5b08c69c3c1a1b94ac746745e005e49fd
Peter-Jacob <46247351+Peter-Jacob@users.noreply.github.com>
2020-05-12 11:05:02 +0200

    changed floor and ceil to use standard C-functions

--- lstring/ceil.c
+++ lstring/ceil.c
@@ -2,23 +2,12 @@
 // Created by PeterJ on 01.05.2020.
 //
 #include "lstring.h"
-
+#include <math.h>
 /* ------------------- Ceil ------------------ */
 void __CDECL
 Lceil( const PLstr to, const PLstr num )
 {
-    double    s;
-    int       nlen;
-    char      fnull;
-  // round number to one fraction digit and create string number
-    snprintf(LSTR(*to), LMAXLEN(*to), "%.*f", (int)1,Lrdreal(num));
-    nlen=STRLEN(LSTR(*to));
-    fnull=LSTR(*to)[nlen-1];     // fetch first fraction digit
-    LLEN(*to)  = nlen-2;         // set string length to represent integer
-
-    LTYPE(*to) = _Lisnum(to);    // convert it to integer and set type (lLastScannedNumber is also set)
-    s=lLastScannedNumber;           // s is created integer
+    LINT(*to)  = ceil(Lrdreal(num));
+    LTYPE(*to) = LINTEGER_TY;
     LLEN(*to)  = sizeof(long);
-    if (fnull != '0' && s > 0) { s++; } // +1 if first fraction was <> 0 and number is positive (CEILING)
-    LINT(*to)  = (long)s;
-  } /* Ceil */
+} /* Ceil */
--- lstring/floor.c
+++ lstring/floor.c
@@ -1,24 +1,13 @@
 //
-// Created by PeterJ on 02.05.2020.
+// Created by PeterJ on 01.05.2020.
 //
 #include "lstring.h"
-
+#include <math.h>
 /* ------------------- Floor ------------------ */
 void __CDECL
 Lfloor( const PLstr to, const PLstr num )
 {
-    double    s;
-    int       nlen;
-    char      fnull;
-    // round number to one fraction digit and create string number
-    snprintf(LSTR(*to), LMAXLEN(*to), "%.*f", (int)1,Lrdreal(num));
-    nlen=STRLEN(LSTR(*to));
-    fnull=LSTR(*to)[nlen-1];     // fetch first fraction digit
-    LLEN(*to)  = nlen-2;         // set string length to represent integer
-
-    LTYPE(*to) = _Lisnum(to);    // convert it to integer and set type (lLastScannedNumber is also set)
-    s=lLastScannedNumber;           // s is created integer
+    LINT(*to)  = floor(Lrdreal(num));
+    LTYPE(*to) = LINTEGER_TY;
     LLEN(*to)  = sizeof(long);
-    if (fnull != '0' && s < 0) { s--; } // +1 if first fraction was <> 0 and number is positive (CEILING)
-    LINT(*to)  = (long)s;
 } /* Floor */
