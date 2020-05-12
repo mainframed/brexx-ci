5679d65164249bbc030479ad85cdb9ce443485c6
Peter-Jacob <46247351+Peter-Jacob@users.noreply.github.com>
2020-05-12 11:54:48 +0200

    corrected ROUND, due the different behaviour of snprintf in MINGW and JCC compiler on MVS. In JCC snprintf does not round a floating point number, the unneeded digits are just stripped off. We therefore add 0.x5 (x is n+1 0) to force a round if necessary.

--- lstring/round.c
+++ lstring/round.c
@@ -8,11 +8,20 @@
 void __CDECL
 Lround( const PLstr to, const PLstr from, long n) {
     int i;
+    double add;
+
     L2REAL(from);
-    if (LREAL(*from)<0) ;
     Lfx(to,n+15);
 
-    snprintf(LSTR(*to), LMAXLEN(*to), "%.*f", (int)n, LREAL(*from));
+    add=5.0;
+    if (LREAL(*from)<0) add=-add;
+    for (i=0;i<=n;i++) add=add/10;
+
+// the PC Version round does not work correctly as snprintf rounds
+// in the MVS version snprintf does not, we therefore need to add 0.xx5
+// which is stripped off by snprintf later
+
+    snprintf(LSTR(*to), LMAXLEN(*to), "%.*f", (int)n, LREAL(*from)+add);
     LTYPE(*to) = LSTRING_TY;
     LLEN(*to)  = STRLEN(LSTR(*to));
 } /* Lround */
